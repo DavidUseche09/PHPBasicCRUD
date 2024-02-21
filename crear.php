14041940a8dcb393a4195c5d2beef9014634e809
DavidUseche09 <daviduseche09@gmail.com>
2024-02-21 13:30:46 -0500

    Actualizar un registro

--- crear.php
+++ crear.php
@@ -24,3 +24,30 @@ if($_POST["operacion"] == "Crear"){
         echo "Registro creado";
     }
 }
+
+if($_POST["operacion"] == "Editar"){
+    $imagen = '';
+    if($_FILES["imagen_usuario"]["name"] != ''){
+        $imagen = subir_imagen();
+    }else{
+        $imagen = $_POST["imagen_usuario_oculta"];
+    }
+
+
+    $stmt = $conexion->prepare("UPDATE usuarios SET nombre=:nombre, apellidos=:apellidos, imagen=:imagen, telefono=:telefono, email=:email WHERE id=:id");
+
+    $resultado = $stmt->execute(
+        array(
+            ':nombre' => $_POST["nombre"],
+            ':apellidos' => $_POST["apellidos"],
+            ':telefono' => $_POST["telefono"],
+            ':email' => $_POST["email"],
+            ':imagen' => $imagen,
+            ':id' => $_POST["id_usuario"]
+        )
+    );
+
+    if (!empty($resultado)){
+        echo "Registro actualizado";
+    }
+}
